e69a641ec6ea476baef0558de9cb17f77bca6b99
Kyleigh <kyleigh.lisi.2020@student.lhschools.org>
2019-09-16 09:51:17 -0400

    Deleted screen line and added modulus

--- src/MathTester.java
+++ src/MathTester.java
@@ -14,10 +14,10 @@ public class MathTester {
         System.out.println(firstWord+(firstNumber+secondNumber));
         System.out.println("firstNumber + secondNumber is " + (firstNumber+secondNumber));
         System.out.println("firstNumber - secondNumber is " + (firstNumber-secondNumber));
-        System.out.println("firstNumber + secondNumber is " + (firstNumber+secondNumber));
         System.out.println("secondNumber / firstNumber is " + (secondNumber/firstNumber));
         System.out.println("firstNumber * secondNumber is " + (firstNumber*secondNumber));
-        
+        System.out.println("The modulus of the secondNumber by the firstNumber " +
+                "is " + (secondNumber%firstNumber));
 
 
 
